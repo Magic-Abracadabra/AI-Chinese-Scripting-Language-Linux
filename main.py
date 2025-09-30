7f10b26b74eab92b72f77d155bfb5aa93028d50a
如意The Magic Wand <you2kai1yuan2@hainanu.edu.cn>
2025-09-30 10:25:43 +0800

    Add the magic-import block

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# magic import
+from pip import main
+from importlib.metadata import distributions
+installed_packages = [dist.metadata['Name'] for dist in distributions()]
+normal_import = __builtins__.__import__
+
+def install(name, globals=None, locals=None, fromlist=(), level=0):
+	__builtins__.__import__ = normal_import
+	if name not in installed_packages:
+		main(['install', '-U', name])
+	name = normal_import(name, globals, locals, fromlist, level)
+	__builtins__.__import__ = install
+	return name
+
+__builtins__.__import__ = install
+
+# main.py
+
 import requests, json, sys, traceback
 from pyautogui import confirm
 
@@ -67,3 +85,4 @@ if len(sys.argv) == 1:
 else:
 	with open(sys.argv[1], encoding='utf-8') as scripts:
 		execute(scripts.read())
+
